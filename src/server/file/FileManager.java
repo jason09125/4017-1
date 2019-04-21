3382850e8ea6d7277964277e2435da1801b0af10
PAN Zhiqi <zhiqipan@yahoo.com>
2019-04-21 12:53:13 +0800

    Support reading user info from database

---
+++ src/server/file/FileManager.java
@@ -0,0 +1,60 @@
+package server.file;
+
+import java.io.*;
+import java.util.HashMap;
+
+public class FileManager {
+  static private String FILE = "./db.txt";
+  // username -> [password, tfaSecret]
+  static private HashMap<String, String[]> userMap = new HashMap<>();
+
+  public static String[] getByKey(String username) {
+    return userMap.get(username);
+  }
+
+  public static void init() {
+    String line;
+
+    BufferedReader bufferReader;
+    try {
+      bufferReader = new BufferedReader(new FileReader(FILE));
+      while ((line = bufferReader.readLine()) != null) {
+        String[] items = line.split("\\s*,\\s*");
+        String username = items[0];
+        String password = items[1];
+        String tfaSecret = items[2];
+        userMap.put(username, new String[]{password, tfaSecret});
+        System.out.printf("[%s]: %s, %s\n", username, password, tfaSecret);
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public static void appendUsers(String[] users) throws IOException {
+    for (String line : users) {
+      String[] items = line.split("/s*,/s*");
+      String username = items[0];
+      String password = items[1];
+      String tfaSecret = items[2];
+      appendUser(username, password, tfaSecret);
+    }
+  }
+
+  public static void appendUser(String username, String password, String tfaSecret) {
+    try {
+      write(username, password, tfaSecret, true);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private static void write(String username, String password, String tfaSecret, boolean append) throws IOException {
+    FileWriter fileWriter = new FileWriter(FILE, append);
+    PrintWriter printWriter = new PrintWriter(fileWriter);
+    printWriter.printf("%s, %s, %s", username, password, tfaSecret);
+    printWriter.println();
+    printWriter.close();
+  }
+
+}
