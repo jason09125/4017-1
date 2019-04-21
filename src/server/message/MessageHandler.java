c8bdeda59cfc739c9235941d071ceab092c3702a
PAN Zhiqi <zhiqipan@yahoo.com>
2019-04-21 23:17:21 +0800

    Fix message encryption procedure

--- src/server/message/MessageHandler.java
+++ src/server/message/MessageHandler.java
@@ -2,15 +2,16 @@ package server.message;
 
 import server.auth.ServerAuthenticator;
 import server.user.UserManager;
-import shared.AsymmetricCrypto;
 import shared.SymmetricCrypto;
 
-public class MessageAgent {
+public class MessageHandler {
+  // todo: split into two functions, so no repeated server signing
   public static byte[] parseFromClientAndGetDeliverable(String fromUsername, String toUsername, byte[] encryptedWithSessionKey) {
     byte[] senderSessionKey = UserManager.getSessionKey(fromUsername, false);
-    byte[] receiverPublicKey = UserManager.getPublicKey(toUsername);
     byte[] fromSigned = SymmetricCrypto.decrypt(encryptedWithSessionKey, senderSessionKey);
     byte[] serverSigned = ServerAuthenticator.encryptWithPrivateKey(fromSigned);
-    return AsymmetricCrypto.encryptWithPublicKey(serverSigned, receiverPublicKey);
+
+    byte[] receiverSessionKey = UserManager.getSessionKey(toUsername, false);
+    return SymmetricCrypto.encrypt(serverSigned, receiverSessionKey);
   }
 }
